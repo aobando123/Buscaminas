835fb68910270884e23ce0c4449968da9461ed79
Mauricio <Mauricio@FamiliaAlvaro>
2017-04-12 18:38:51 -0600

    Metodo Restart

--- src/buscaminasPackage/Rutinas.java
+++ src/buscaminasPackage/Rutinas.java
@@ -16,10 +16,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
 import java.util.Random;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
